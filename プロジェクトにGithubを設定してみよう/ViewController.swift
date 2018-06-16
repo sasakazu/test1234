6b3f2b3cf33978edb76b1c455933db407ce5a7fc
sasakazu <sasakura.company@gmail.com>
2018-06-16 13:09:44 +0900

    first commit

---
+++ プロジェクトにGithubを設定してみよう/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  プロジェクトにGithubを設定してみよう
+//
+//  Created by 笹倉 一也 on 2018/06/16.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
